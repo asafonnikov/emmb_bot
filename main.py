dd62c54db1c21ede79a01f29bb08e86ce4e2f98b
floi <asafonnikov@gmail.com>
2024-11-11 15:02:27 +0300

    Add report about false deleting

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ badWords = [i.rstrip() for i in badWords]
 sosoWords = readFile("sosoWords")
 sosoWords = [i.rstrip() for i in sosoWords]
 
+# Последнии удаления пользователя
+lastDelete = []
+
 # Удалет все подряд повторяющие сообщения
 def toUniqueSymbols(msg):
     newMsg = ""
@@ -67,6 +70,15 @@ def isBadMsg(msg):
     # Если больше 3 не очень слов то нельзя
     return countMatches(msg, sosoWords) > 2
 
+# Сохраним последнее удалёное сообщение от пользователя, для возможной дальнейшей обратной связи
+def saveLastDelete(user, msg):
+    for i in lastDelete:
+        if i[0] != user:
+            continue
+        lastDelete.remove(i)
+        break
+    lastDelete.append([user, msg])
+
 @bot.message_handler(func=lambda m: True)
 def msgHandle(message):
     msg = message.text
@@ -74,18 +86,29 @@ def msgHandle(message):
 
     if msg.startswith("/report"):
         if message.reply_to_message == None:
-            bot.reply_to(message, "Ошибка! Вы должны отвечать на сообщение для его репорта")
+            for i in lastDelete:
+                if i[0] != user:
+                    continue
+                bot.reply_to(message, "Последнее удаленое сообщение от вас помечено как ложное. Спасибо за обратную связь")
+                floiLog.log(f"Пользователь {user} пометил своё последнее удалёное сообщение '{i[1]}' как ложное")
+                return
+            
+            bot.reply_to(message, "Ошибка! Ни одно ваше сообщение не было удалено")
             return
-        floiLog.log(f"Пользователь {user} пометил сообщение {message.reply_to_message.text} как потенциально недопустимое")
+        
+        floiLog.log(f"Пользователь {user} пометил сообщение '{message.reply_to_message.text}' как потенциально недопустимое")
         bot.reply_to(message, "Сообщение отправлено на дальнейщую проверку. Спасибо за обратную связь")
     
     if len(msg) > 100:
         floiLog.log(f"ДЛИННОЕ {user}: {message.text}")
         bot.reply_to(message, "Сообщение сочтено потенциально не допустимым, тк является слишком длинное")
+        saveLastDelete(user, msg)
+    
     elif isBadMsg(msg):
         floiLog.log(f"НЕНОРМАТИВНОЕ {user}: {message.text}")
         bot.reply_to(message, "Сообщение сочтено потенциально не допустимым, тк содержит ненормативную лексику")
-
+        saveLastDelete(user, msg)
+        
 
 bot.infinity_polling()
 
